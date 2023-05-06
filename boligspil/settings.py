671a2e0a17b6e3ebd38a137bf3c3e2f5257f9b9e
kristian2890 <30598398+kristian2890@users.noreply.github.com>
2023-05-06 22:51:15 +0200

    adjusted env

--- boligspil/settings.py
+++ boligspil/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("DJANGO_SECRET_KEY", default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", default=False) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default=None)
 
 
 # Application definition
